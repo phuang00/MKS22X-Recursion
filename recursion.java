2f0a9589836cfe3b78aeb9abc6e6684ad5812220
phuang00 <phuang00@stuy.edu>
2019-01-31 14:22:49 -0500

    wrote sqrt and helper method and tested in main

--- recursion.java
+++ recursion.java
@@ -7,19 +7,34 @@ public class recursion{
 
     */
     public static double sqrt(double n, double tolerance){
+      if (n == 0) return 0;
+      return sqrtH(n, 1, tolerance);
+    }
 
+    private static double sqrtH(double n, double guess, double tolerance){
+      if (Math.abs((Math.pow(guess, 2) - n) / n) * 100.0 <= tolerance) return guess;
+      return sqrtH(n, (n / guess + guess) / 2, tolerance);
     }
 
     /*Recursively find the n'th fibbonaci number in linear time
      *fib(0) = 1; fib(1) = 1; fib(5) = 5
      *precondition: n is non-negative
      */
-    public static int fib(int n){
+    /*public static int fib(int n){
 
     }
 
     /*As Per classwork*/
-    public static ArrayList<Integer> makeAllSums(){
-    }
+    /*public static ArrayList<Integer> makeAllSums(){
+    }*/
 
+    public static void main(String[] args) {
+      System.out.println(sqrt(100, 0.0001));
+      System.out.println(sqrt(0, 0.0001));
+      System.out.println(sqrt(1, 0.0001));
+      System.out.println(sqrt(20, 0.0001));
+      System.out.println(sqrt(36, 0.0001));
+      System.out.println(sqrt(10, 0.0001));
+      System.out.println(sqrt(50, 0.0001));
+    }
 }
