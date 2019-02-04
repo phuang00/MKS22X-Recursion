869b5bfc1dcdff93ab17052b4aba7ab63da3f6b8
phuang00 <phuang00@stuy.edu>
2019-02-03 21:44:19 -0500

    added test cases from Timothy So's driver on google groups

--- recursion.java
+++ recursion.java
@@ -94,5 +94,65 @@ public class recursion{
       System.out.println("makeAllSums(2): \n  (should print [3, 2, 1, 0]) \n\t\t" + makeAllSums(2));
       System.out.println("makeAllSums(1): \n  (should print [1, 0]) \n\t\t" + makeAllSums(1));
       System.out.println("makeAllSums(0): \n  (should print [0]) \n\t\t" + makeAllSums(0));
+      System.out.println();
+      System.out.println();
+      System.out.println("sqrt()");
+      System.out.println("-----------------------\n");
+
+      System.out.println("sqrt(4, 0.00001)");
+      System.out.println("EXPECTED: 2");
+      System.out.println(sqrt(4, 0.00001));         // 2
+
+      System.out.println("----");
+
+      System.out.println("sqrt(8, 0.00001)");
+      System.out.println("EXPECTED: ~2.8284");
+      System.out.println(sqrt(8, 0.00001));         // ~2.8284
+
+      System.out.println("----");
+
+      System.out.println("sqrt(0, 0.00001)");
+      System.out.println("EXPECTED: 0");
+      System.out.println(sqrt(0, 0.00001));         // 0
+
+      System.out.println("\n");
+
+      System.out.println("fib()");
+      System.out.println("-----------------------\n");
+
+      System.out.println("fib(0)");
+      System.out.println("EXPECTED: 0");
+      System.out.println(fib(0));                   // 0
+
+      System.out.println("----");
+
+      System.out.println("fib(1)");
+      System.out.println("EXPECTED: 1");
+      System.out.println(fib(1));                   // 1
+
+      System.out.println("----");
+
+      System.out.println("fib(7)");
+      System.out.println("EXPECTED: 13");
+      System.out.println(fib(7));                   // 13
+
+      System.out.println("makeAllSums()");
+      System.out.println("-----------------------\n");
+
+      System.out.println("makeAllSums(0)");
+      System.out.println("EXPECTED: [0]");
+      System.out.println(makeAllSums(0));           // [0]
+
+      System.out.println("----");
+
+      System.out.println("makeAllSums(1)");
+      System.out.println("EXPECTED: [1, 0]");
+      System.out.println(makeAllSums(1));           // [1, 0]
+
+      System.out.println("----");
+
+      System.out.println("makeAllSums(3)");
+      System.out.println("EXPECTED: [0, 3, 2, 5, 1, 4, 3, 6]");
+      System.out.println(makeAllSums(3));           // [0, 3, 2, 5, 1, 4, 3, 6]
     }
 }
